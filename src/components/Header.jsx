0a059b08e275d0786993e846392aca2bc3fa6d82
Maxwell Novak <novak.max@gmail.com>
2020-03-15 13:18:41 -0400

    Make header point to sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
   background-color: #60BBBF;
   margin: 0 auto;
   maxWidth: 960;
@@ -14,19 +15,51 @@ const H1 = styled.h1`
   margin: 0;
 `;
 
+const Links = styled.div`
+  margin: auto 0;
+  color: #FFFFFF;
+`;
+
+const Link = styled.a`
+  color: #FFFFFF;
+  text-decoration: none;
+  margin: 0 10px;
+`;
+
 export const Header = ({ siteTitle }) => (
   <Container>
     <H1>
       <Link
-        to="/"
-        style={{
-          color: "white",
-          textDecoration: "none",
-        }}
+        href="/"
       >
         {siteTitle}
       </Link>
     </H1>
+    <Links>
+      <Link
+        href="/"
+      >
+        Home
+      </Link>
+      |
+      <Link
+        href="/#projects"
+      >
+        Projects
+      </Link>
+      |
+      <Link
+        href="/#streaming"
+      >
+        Streaming
+      </Link>
+      |
+      <Link
+        href="/#about"
+      >
+        About
+      </Link>
+    </Links>
   </Container>
 );
 
