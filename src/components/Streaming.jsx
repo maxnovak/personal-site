fe295cb326ea44799aaa1a9995f8baf36ba2725f
Maxwell Novak <novak.max@gmail.com>
2020-04-25 17:06:54 -0400

    Remove eventlistener

--- src/components/Streaming.jsx
+++ src/components/Streaming.jsx
@@ -12,20 +12,13 @@ export const Streaming = () => {
     script.src = "https://embed.twitch.tv/embed/v1.js";
     script.defer = true;
     script.addEventListener("load", () => {
-       
-      var embed= new window.Twitch.Embed("streaming",
-        {
-          channel: "maxamander",
-          width: "100%",
-          height: 800,
-        });
-      embed.addEventListener(window.Twitch.Embed.VIDEO_READY, () => {
 
-        // eslint-disable-next-line no-console
-        console.log("The video is ready");
-      
+    new window.Twitch.Embed("streaming",
+      {
+        channel: "maxamander",
+        width: "100%",
+        height: 800,
       });
-    
     });
 
     document.body.appendChild(script);
